6ff08cc7ab5ad8c7ffa81c77814a73c65e6467f4
Condorbox <minecraft13122001@gmail.com>
2023-09-17 12:12:40 +0900

    on_progress callback

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import pytube
+from pytube.cli import on_progress
 from pytube.exceptions import AgeRestrictedError
 from pydub import AudioSegment
 
 client_id = os.environ.get("CLIENT_ID")
 client_secret = os.environ.get("CLIENT_SECRET")
 redirect_uri = os.environ.get("REDIRECT_URI")
-
-'''sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id, client_secret, redirect_uri, scope="playlist-read-private"))
-
 playlist_id = os.environ.get("PLALIST_ID")
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id, client_secret, redirect_uri, scope="playlist-read-private"))
 playlist = sp.playlist_tracks(playlist_id)
 
 for track in playlist["items"]:
@@ -21,16 +21,14 @@ for track in playlist["items"]:
 
     search = pytube.Search(f"{track_name}, {artists}")
     video_url = f"https://www.youtube.com/watch?v={search.results[0].video_id}"
-    yt = pytube.YouTube(url=video_url, use_oauth=True, allow_oauth_cache=True, )
+    yt = pytube.YouTube(url=video_url, use_oauth=True, allow_oauth_cache=True, on_progress_callback=on_progress)
     try :
         audio_stream = yt.streams.filter(only_audio=True, file_extension='mp4').first()
         output_path = "./songs"
         audio_stream.download(output_path=output_path)
-        print(f"Dowload {track_name}: {output_path}")
+        print(f"Dowload {track_name} to {output_path}")
     except AgeRestrictedError as e:
         print(f"{track_name} ERROR: age restricted -> {e}")
-
-print("End of the program")'''
 
 SONGS_DIR = ".\songs"
 OUTPUT_DIR = ".\ogg_songs"
